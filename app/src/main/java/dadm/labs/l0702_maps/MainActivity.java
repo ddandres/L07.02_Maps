7b6dca85f479532206776b28ce7099111d99172a
ddandres <ddandres@disca.upv.es>
2021-12-30 13:25:27 +0100

    Updated to API 31

--- app/src/main/java/dadm/labs/l0702_maps/MainActivity.java
+++ app/src/main/java/dadm/labs/l0702_maps/MainActivity.java
@@ -19,6 +19,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -34,13 +35,11 @@ import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.List;
 
-/**
- * Displays the location of the SDM lab as a marker on Google Maps within a SupportMapFragment.
- * New markers can be added through their longitudes and latitudes.
- * Human readable addresses are obtained and displayed in a custom InfoWindow when markers are clicked.
- * If the InfoWindow is clicked, then the route from DADM lab to that location is obtained through
- * the Goggle Maps Directions API, and it is displayed on the map using the Maps Util library.
- */
+// Displays the location of the SDM lab as a marker on Google Maps within a SupportMapFragment.
+// New markers can be added through their longitudes and latitudes.
+// Human readable addresses are obtained and displayed in a custom InfoWindow when markers are clicked.
+// If the InfoWindow is clicked, then the route from DADM lab to that location is obtained through
+// the Goggle Maps Directions API, and it is displayed on the map using the Maps Util library.
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
 
     // Hold reference to the GoogleMap used to display location information
@@ -68,22 +67,23 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         bAddMarker = findViewById(R.id.bAddMarker);
         progressBar = findViewById(R.id.progressBar);
 
-        // Keep a reference to the fragment displaying the map
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        // Wait for the map to be ready before adding any marker or route
-        mapFragment.getMapAsync(this);
+        findViewById(R.id.bAddMarker).setOnClickListener(v -> translateCoordinates());
 
+        // Keep a reference to the fragment displaying the map
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fcvMap);
+        // Wait for the map to be ready before adding any marker or route
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
         // Adapter for the custom InfoWindow
         infoWindowAdapter = new MyInfoWindowAdapter();
     }
 
-    /**
-     * This method is called whenever the button to add a marker on the map is clicked.
-     * It launches an AsyncTask to translate longitude and latitude into a human readable address.
-     *
-     * @param view View that generated the event (useless in this case)
-     */
-    public void translateCoordinates(View view) {
+    // This method is called whenever the button to add a marker on the map is clicked.
+    // It launches an AsyncTask to translate longitude and latitude into a human readable address.
+    // view View that generated the event (useless in this case)
+    private void translateCoordinates() {
 
         double lat, lng;
 
@@ -104,15 +104,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    /**
-     * This method adds a new marker to the map.
-     *
-     * @param latitude  Latitude where the marker should be located.
-     * @param longitude Longitude where the marker should be located.
-     * @param title     Title of the InfoWindow associated to the marker.
-     * @param snippet   Description to be displayed in the associated InfoWindow.
-     * @param color     Color of the marker on the map.
-     */
+    // This method adds a new marker to the map.
+    // latitude  Latitude where the marker should be located.
+    // longitude Longitude where the marker should be located.
+    // title     Title of the InfoWindow associated to the marker.
+    // snippet   Description to be displayed in the associated InfoWindow.
+    // color     Color of the marker on the map.
     private void addMarker(double latitude, double longitude, String title, String snippet, float color) {
         MarkerOptions options = new MarkerOptions();
         options.position(new LatLng(latitude, longitude));
@@ -123,11 +120,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         map.addMarker(options);
     }
 
-    /**
-     * This method is executed when the activity is created to populate the ActionBar with actions.
-     */
+    // This method is executed when the activity is created to populate the ActionBar with actions.
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
 
         // Add the menu to the ActionBar only if the map has been initialized.
         if (map != null) {
@@ -136,9 +131,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return true;
     }
 
-    /**
-     * This method is executed when any action from the ActionBar is selected.
-     */
+    // This method is executed when any action from the ActionBar is selected.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -157,9 +150,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return true;
     }
 
-    /**
-     * Check whether Internet connectivity is available.
-     */
+    // Check whether Internet connectivity is available.
     private boolean isConnectionAvailable() {
 
         // Get a reference to the ConnectivityManager
@@ -170,15 +161,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return ((info != null) && info.isConnected());
     }
 
-    /**
-     * This method is called whenever the Google;ap is ready to be used.
-     *
-     * @param googleMap The initialized GoogleMap object.
-     *                  You should keep a reference to this object to manipulate the map,
-     *                  add markers to it, and so on.
-     */
+    // This method is called whenever the Google;ap is ready to be used.
+    // googleMap The initialized GoogleMap object.
+    // You should keep a reference to this object to manipulate the map,
+    // add markers to it, and so on.
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
 
         // Keep a reference to the initialized GoogleMap object
         map = googleMap;
@@ -200,13 +188,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         bAddMarker.setVisibility(View.VISIBLE);
     }
 
-    /**
-     * This method will be called whenever an InfoWindow is clicked.
-     *
-     * @param marker Marker that was clicked.
-     */
+    // This method will be called whenever an InfoWindow is clicked.
+    // marker Marker that was clicked.
     @Override
-    public void onInfoWindowClick(Marker marker) {
+    public void onInfoWindowClick(@NonNull Marker marker) {
 
         // Check whether the Internet connection is available
         if (isConnectionAvailable()) {
@@ -215,14 +200,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    /**
-     * Custom InfoWindowAdapter to display the name of the place represented by the marker,
-     * its address, and its location coordinates.
-     */
+    // Custom InfoWindowAdapter to display the name of the place represented by the marker,
+    // its address, and its location coordinates.
     private class MyInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
         @Override
-        public View getInfoWindow(Marker marker) {
+        public View getInfoWindow(@NonNull Marker marker) {
             return null;
         }
 
